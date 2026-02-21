56af37b04809cc6f33a57766d78d167282191949
Mathias Bergholt Buhl <mbbu@universal-robots.com>
2026-02-21 14:33:02 +0100

    Feature to delete tasks from ToDo-list Fixes #5

--- main.cpp
+++ main.cpp
@@ -47,23 +47,41 @@ int main() {
 		std::cout << "\nwrite a task" << std::endl;
 		std::string command = "";
 		std::getline(std::cin, command);
+		//View ToDo-list
 		if (command[0] == '1')
 		{
 			backlog.viewTasks();
 		}
+		//Add task to ToDo-list
 		else if (command[0] == '2' && command.size() > 2)
 		{
 			backlog.tasklist.push_back(command.substr(2));
 		}
+		//Edit a specific task from ToDo-list
 		else if (command[0] == '3')
 		{
 			std::cout << "write number of task you wish to edit\nfollowed by what you want to change it to" << std::endl;
+			backlog.viewTasks();
 			std::getline(std::cin, command);
 			for (int i = 0; i < backlog.tasklist.size(); ++i)
 			{
 				if ((command[0] - '0') == i && command.size() > 2)
 				{
 					backlog.tasklist[i]._description = command.substr(2);
+				}
+			}
+		}
+		//Delete a specific task from ToDo-list
+		else if (command[0] == '4')
+		{
+			std::cout << "write which task you wish to delete" << std::endl;
+			backlog.viewTasks();
+			std::getline(std::cin, command);
+			for (int i = 0; i < backlog.tasklist.size(); ++i)
+			{
+				if ((command[0] - '0') == i)
+				{
+					backlog.tasklist.erase(backlog.tasklist.begin()+i);
 				}
 			}
 		}
